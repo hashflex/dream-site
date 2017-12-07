752b6da46a1fb06b4266d7017dbe0b19e0c3a6f7
Amiya Sahu <amiya053@gmail.com>
2017-12-06 23:44:14 -0500

    binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,8 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector : 'app-server',
-	templateUrl : 'server.component.html'
+	templateUrl : 'server.component.html',
+  	styleUrls: ['./server.component.css']
 }) // this is just a decorator
 export class ServerComponent {
-	
+	serverID: number = 10;
+	status: string = 'Offline';
+
+	public getStatus() : string {
+		return this.status;
+	}
 }

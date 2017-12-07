d69a98d14a631b4927566c9a740b16f17b4d2cf4
Amiya Sahu <amiya053@gmail.com>
2017-12-06 23:07:11 -0500

    created 2 new components

---
+++ src/app/success-alert/successalert.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'success-alert',
+  templateUrl: './successalert.component.html',
+  styleUrls: ['./successalert.component.css']
+})
+export class SuccessAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

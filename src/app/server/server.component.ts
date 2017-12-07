b3ad793a5bdaa3a74f57b532e49bc25ed3b460ab
Amiya Sahu <amiya053@gmail.com>
2017-12-06 21:54:54 -0500

    Created server component

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector : 'app-server',
+	templateUrl : 'server.component.html'
+}) // this is just a decorator
+export class ServerComponent {
+	
+}

1f8d2403c5f8430080fa8455205b43bb35eedb53
Amiya Sahu <amiya053@gmail.com>
2017-12-09 17:01:45 -0500

    added ngClass and ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,10 +6,22 @@ import { Component } from '@angular/core';
   	styleUrls: ['./server.component.css']
 }) // this is just a decorator
 export class ServerComponent {
-	serverID: number = 10;
+	serverID: number = Math.floor(Math.random() * 90 + 10);
 	status: string = 'Offline';
+
+	constructor(){
+		this.status = Math.random() > 0.5 ? 'Online' : 'Offline';
+	}
 
 	public getStatus() : string {
 		return this.status;
+	}
+
+	getClass(){
+		return this.status === 'Online' ? 'm-3 p-3 bg-success' : 'm-3 p-3 bg-danger';
+	}
+
+	getColor(){
+		return this.status === 'Online' ? 'white' : 'black';
 	}
 }
